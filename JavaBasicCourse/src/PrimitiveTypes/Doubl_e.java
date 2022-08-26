498f5e4e91b6e233a69aeb0509afc4dc5753bf1d
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-08-26 23:09:53 +0300

    double

---
+++ JavaBasicCourse/src/PrimitiveTypes/Doubl_e.java
@@ -0,0 +1,10 @@
+package PrimitiveTypes;
+
+public class Doubl_e {
+    public static void main(String[] args) {
+        System.out.println(0x0bp3);
+        Object N = 0x0bp3;
+        System.out.println(N.getClass());
+    }
+
+}
