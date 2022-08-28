2469b939b619318480cf9feaa50d0fcd4e4fbefc
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-08-28 23:16:43 +0300

    Unico

---
+++ JavaBasicCourse/src/PrimitiveTypes/Unicode.java
@@ -0,0 +1,30 @@
+package PrimitiveTypes;
+
+public class Unicode {
+
+    public static char charExpression(int a) {
+        char ch = '\\';
+        /*char x = (char) (ch + a);*/
+        // или
+        ch += a;
+        return ch;
+    }
+
+    public static void main(String[] args) {
+
+       /* char c = '^';
+        int u = (int) c;
+        System.out.println(u);
+        char ch, ch_a, ch_d ;
+        int b = 92;                     // Представление символа Х в коде ASCI I
+        int a = 93;
+        int d = 94;
+        ch = ( char) b ;            //  Явное приведение несовместимых типов
+        ch_a = ( char) a ;
+        ch_d = ( char) d ;
+        System.out.println("ch: " + ch);
+        System.out.println("ch_a: " + ch_a);
+        System.out.println("ch_d: " + ch_d);*/
+        System.out.println(charExpression(1));
+    }
+}
