6abfacf0c2b1d43067a81c36029f5f175de68a28
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-08-30 22:25:59 +0300

    Types

---
+++ JavaBasicCourse/src/PrimitiveTypes/GalToLit.java
@@ -0,0 +1,31 @@
+/**
+ * Gallons to liters program
+ *
+ * @author Valentyn Mozul
+ * @version 1.0
+ */
+
+package PrimitiveTypes;
+
+public class GalToLit {
+    /**
+     * Program entry point.
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        double gallons;
+
+        double liters;
+
+        double LitersOnGallons;
+
+        gallons = 10;
+
+        liters = 3.7854;
+
+        LitersOnGallons = gallons * liters;
+
+        System.out.println("10 gallons = " + LitersOnGallons + " liters");
+    }
+
+}
---
+++ JavaBasicCourse/src/PrimitiveTypes/TypeConversions.java
@@ -0,0 +1,16 @@
+package PrimitiveTypes;
+
+public class TypeConversions {
+
+    public static void main(String[] args) {
+       /* long -> float;+
+        int -> long; +
+        String -> int;
+        byte -> char;
+        char -> int; +
+        int -> boolean;
+        char -> Character; +
+        float -> long;*/
+
+    }
+}
