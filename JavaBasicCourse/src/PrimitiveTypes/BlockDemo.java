cd0431b9e5119b6b8846be811d3a3e1c9b6ad5f3
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-08-31 17:37:18 +0300

    FOR AND BLOCK

---
+++ JavaBasicCourse/src/PrimitiveTypes/BlockDemo.java
@@ -0,0 +1,26 @@
+/**
+ * Demonstration of code blocks
+ *
+ * @author Valentyn Mozul
+ * @version 1.0
+ */
+
+package PrimitiveTypes;
+
+public class BlockDemo {
+    /**
+     * Program entry point.
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        double i, j, d;
+        i = 5;
+        j = 10;
+
+        if(i != 0){
+            System.out.println(" i не равно нулю");
+            d = j / i;
+            System.out.println("j / i равно " + d);
+        }
+    }
+}
---
+++ JavaBasicCourse/src/PrimitiveTypes/ForDemo.java
@@ -0,0 +1,22 @@
+/**
+ * Demonstration of the use of the for loop
+ *
+ * @author Valentyn Mozul
+ * @version 1.0
+ */
+
+package PrimitiveTypes;
+
+public class ForDemo {
+    /**
+     * Program entry point.
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        int count;
+        for (count = 0; count < 5; count++){
+            System.out.println("Знaчeниe счетчика : " + count);
+        }
+        System.out.println("Гoтoвo!");
+    }
+}
