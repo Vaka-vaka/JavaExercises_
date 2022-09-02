c05a440eaac2e528ed28d4e562164f86df3ab8e9
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-09-03 00:08:27 +0300

    Moon

---
+++ JavaBasicCourse/src/PrimitiveTypes/Moon.java
@@ -0,0 +1,25 @@
+/**
+ * Gravity on the Moon
+ *
+ * @author Valentyn Mozul
+ * @version 1.0
+ */
+
+/*Сила тяжести на Луне составляет около 17% земной. Напишите программу,
+которая вычислила бы ваш вес на Луне.*/
+
+package PrimitiveTypes;
+
+public class Moon {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        double weight_Moon;
+        double weight = 106.6;
+        weight_Moon = (weight * 0.17);
+        System.out.println("My weight on the moon - " + weight_Moon + " kg.");
+    }
+}
