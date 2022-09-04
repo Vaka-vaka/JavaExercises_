3c75433c1f7da4bc8a1759ba335de47717845ac6
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-09-04 19:58:23 +0300

    Create InchesIntoMeters.java

---
+++ JavaBasicCourse/src/PrimitiveTypes/InchesIntoMeters.java
@@ -0,0 +1,39 @@
+/**
+ * Translating inches into meters
+ *
+ * @author Valentyn Mozul
+ * @version 1.0
+ */
+
+package PrimitiveTypes;
+
+public class InchesIntoMeters {
+    /**
+     * Program entry point.
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        double inch, meter;
+        int counter;
+        String a = "a";
+
+        counter = 0;
+        for(inch = 1; inch <= 100; inch++){
+            meter = inch * 39.37;
+            counter++;
+            if(counter == 1)
+                System.out.println(inch + " дюйм" + " = " + meter + " метрa");
+            if(counter == 2)
+                System.out.println(inch + " дюйм" + a + " = " + meter + " метрa");
+            if(counter == 5)
+                System.out.println(inch + " дюймов" + " = " + meter + " метрa");
+
+           //System.out.println(inch + " дюйм" + a + " = " + meter + " метрa");
+
+            if(counter == 12){
+                System.out.println();
+                counter = 0;
+            }
+        }
+    }
+}
