c4ece09d601398e46549a799bfbd8ffba46617ce
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-09-04 19:58:20 +0300

    Update GalToLitTaЬle.java

--- JavaBasicCourse/src/PrimitiveTypes/GalToLitTaЬle.java
+++ JavaBasicCourse/src/PrimitiveTypes/GalToLitTaЬle.java
@@ -20,7 +20,7 @@ public class GalToLitTaЬle {
         counter = 0;
         for (gallons = 1; gallons <= 100; gallons++) {
             liters = gallons * 3.7854; // преобразование в литры
-            System.out.println(gallons + " галлонам соответ ствует " +
+            System.out.println(gallons + " галлонам соответствует " +
                     liters + " литра.");
             counter++; // Увеличивать значение счетчика строк на 1 на каждой итерации цикла
             if (counter == 10) { // Если значение счетчика равно 1О, вывести пустую строку
