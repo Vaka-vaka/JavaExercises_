f2e925daf886a35ca51b7960da0087f385d74772
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-09-30 15:16:32 +0300

    Сhapter_2 30.09.22

---
+++ JavaBasicCourse/src/chapter_4/MyClass.java
@@ -0,0 +1,30 @@
+/**
+ * Simple Designer
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 28.09.2022
+ */
+
+package chapter_4;
+
+public class MyClass {
+    int x;
+
+    MyClass(){
+        x = 10;
+    }
+}
+
+class ConsDemo{
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+    MyClass t1 = new MyClass();
+    MyClass t2 = new MyClass();
+
+        System.out.println(t1.x + " " + t2.x);
+    }
+}
---
+++ JavaBasicCourse/src/chapter_4/MyClass2.java
@@ -0,0 +1,30 @@
+/**
+ * Parameterized constructor
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 28.09.2022
+ */
+
+package chapter_4;
+
+public class MyClass2 {
+    int x;
+
+    MyClass2(int i){
+        x = i;
+    }
+}
+
+class ParmConsDemo {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        MyClass2 t1 = new MyClass2(12);
+        MyClass2 t2 = new MyClass2(22);
+
+        System.out.println(t1.x + " " + t2.x);
+    }
+}
