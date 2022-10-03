398dc06660615b624eab7b3f6636bd84d4fbc5cf
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-10-03 15:12:00 +0300

    Сhapter_2 03.10.22

---
+++ JavaBasicCourse/src/chapter_5/ArrayDemo.java
@@ -0,0 +1,27 @@
+/**
+ * Demonstration of a one-dimensional array
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 01.10.2022
+ */
+
+package chapter_5;
+
+public class ArrayDemo {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        int[] sample = new int[10];
+        int i;
+
+        for(i = 0; i < 10; i++){
+            sample[i] = i;
+        }
+        for(i = 0; i < 10; i++){
+            System.out.println("Элeмeнт sample [ " + i + " ] : "+ sample [i]);
+        }
+    }
+}
