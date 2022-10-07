8b5cfde638aae94824c0c9c1008d7ee7659bd3c1
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-10-07 15:41:22 +0300

    Сhapter_2 07.10.22

---
+++ JavaBasicCourse/src/chapter_5/ACopy.java
@@ -0,0 +1,31 @@
+/**
+ * An example of using the length variable to copy arrays
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 05.10.2022
+ */
+
+package chapter_5;
+
+public class ACopy {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+        int[] nums = new int[10];
+        int[] nums2 = new int[10];
+
+        for (i = 0; i < nums.length; i++)
+            nums[i] = i;
+
+        // Копирование массива nums в массив nums2
+        if(nums2.length >= nums.length)
+            for (i = 0; i < nums2.length; i++)
+                nums2[i] = nums[i];
+        for (i = 0; i < nums2.length;i++)
+            System.out.print(nums2[i] + " ");
+    }
+}
