cf1f1321aafe5d4cc9422ff064ef3a8c9fda42b7
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-10-16 18:22:07 +0300

    Сhapter_2 03.10.22

---
+++ JavaBasicCourse/src/chapter_5/TwoD.java
@@ -0,0 +1,27 @@
+/**
+ * Demonstration of the use of a two-dimensional array
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 04.10.2022
+ */
+
+package chapter_5;
+
+public class TwoD {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        int t, i;
+        int[][] table = new int[3][4];
+        for (t = 0; t < 3; ++t){
+            for (i = 0; i < 4; ++i){
+                table[t][i] = (t * 4) + i + 1;
+                System.out.print(table[t][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
