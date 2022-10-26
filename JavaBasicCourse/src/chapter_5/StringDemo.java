2d04a8942c32cf1e305a3a411f12170a8b76ff3f
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-10-26 12:26:37 +0300

    Сhapter_2 26.10.22

---
+++ JavaBasicCourse/src/chapter_5/StringDemo.java
@@ -0,0 +1,28 @@
+/**
+ * Introduction to the String class
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 24.10.2022
+ */
+
+package chapter_5;
+
+public class StringDemo {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        // Различные способы объявления строк
+        String str1 = new String(" B Java строки - это объекты.");
+        String str3 = "Их можно создавать разными способами.";
+        String str2 = new String(str1);
+        String str4 = new String(str3);
+
+        System.out.println(str1);
+        System.out.println(str3);
+        System.out.println(str2);
+        System.out.println(str4);
+    }
+}
