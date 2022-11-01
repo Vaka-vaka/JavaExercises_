3c0469fc7b35359d43cb7e6766ee30e9b8eecc41
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-11-01 16:10:48 +0200

    Сhapter_2 01.11.22

--- JavaBasicCourse/src/chapter_5/StringArrays.java
+++ JavaBasicCourse/src/chapter_5/StringArrays.java
@@ -16,13 +16,35 @@ public class StringArrays {
         }
         return st;
     }
+
+    // Изменение строки
+    String arrayDemo2(String[] att){
+        att[2] = "также является";
+        att[3] = "тестом!";
+        String str ="";
+
+        for (String z : att){
+            str += z + " ";
+        }
+        return str;
+    }
 }
 
 class StringArrayDemo{
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
     public static void main(String[] args) {
         StringArrays stringArrays = new StringArrays();
         String str = stringArrays.arraysDemo(new String[]{"Эта", "строка", "является", "тестом." });
         System.out.println();
         System.out.println(str);
+
+        StringArrays stringArrays1 = new StringArrays();
+        String str1 = stringArrays1.arrayDemo2(new String[]{"Эта", "строка", "является", "тестом."});
+        System.out.println();
+        System.out.print(str1);
     }
 }
