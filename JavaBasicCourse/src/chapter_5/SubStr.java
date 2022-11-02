42412df340e6d28873d954a9d5d8714808d71907
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-11-02 15:14:28 +0200

    Сhapter_2 02.11.22

---
+++ JavaBasicCourse/src/chapter_5/SubStr.java
@@ -0,0 +1,26 @@
+/**
+ * Using the substring method ()
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 28.10.2022
+ */
+
+package chapter_5;
+
+public class SubStr {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        String orgstr = "Java - двигатель Интернета.";
+
+        //Сформировать подстроку
+
+        String substr = orgstr.substring(7, 26);
+
+        System.out.println("orgstr : " + orgstr);
+        System.out.println("substr : " + substr);
+    }
+}
