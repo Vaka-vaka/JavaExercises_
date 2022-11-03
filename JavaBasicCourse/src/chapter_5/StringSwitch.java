a55b61118459e2da4d1b8b168bb1ca019616ca5c
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-11-03 15:07:04 +0200

    Сhapter_2 03.11.22

---
+++ JavaBasicCourse/src/chapter_5/StringSwitch.java
@@ -0,0 +1,34 @@
+/**
+ *  Using strings to control the switch instruction
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 01.11.2022
+ */
+
+package chapter_5;
+
+public class StringSwitch {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        String comand = "connect";
+
+        switch(comand){
+            case "connect":
+                System.out.println("Пoдключeниe");
+                break;
+            case "cancel":
+                System.out.println("Oтмeнa");
+                break;
+            case "disconnect":
+                System.out.println("Oтключeниe");
+                break;
+            default:
+                System.out.println("Heвepнaя команда!");
+                break;
+        }
+    }
+}
