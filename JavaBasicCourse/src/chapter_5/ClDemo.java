6ac95978ad04dbbf1ebf6902299c3a629127fd58
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-11-04 14:32:12 +0200

    Сhapter_2 04.11.22

---
+++ JavaBasicCourse/src/chapter_5/ClDemo.java
@@ -0,0 +1,24 @@
+/**
+ * Display of all data specified on the command line
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 01.11.2022
+ */
+
+package chapter_5;
+
+public class ClDemo {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Пpoгpaммe передано " + args.length + " аргумента командной строки.");
+
+        System.out.println("Список аргументов : ");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args [" + i + "] : " + args[i]);
+        }
+    }
+}
