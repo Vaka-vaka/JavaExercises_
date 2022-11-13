1a5d100cea7d0f1d7b7d18c8599b32a882ea33c9
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-11-13 13:29:16 +0200

    Сhapter_2 11.11.22

---
+++ JavaBasicCourse/src/chapter_5/NoZeroDiv.java
@@ -0,0 +1,26 @@
+/**
+ * Предотвраще ние деления на нуль с помощью оператора ?
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 08.11.2022
+ */
+
+package chapter_5;
+
+public class NoZeroDiv {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        int result;
+
+        for (int i = -5; i < 6; i++){
+            result = i != 0 ? 100 / i:0;
+            if (i != 0){
+                System.out.println("1OO /" + i +" равно " + result);
+            }else System.out.println("Zero " + "= " + i);
+        }
+    }
+}
