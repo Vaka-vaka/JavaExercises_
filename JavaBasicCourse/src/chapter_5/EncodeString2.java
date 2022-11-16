95f454d69d710d44d985aa76318679f66c3eee5f
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-11-16 12:03:30 +0200

    Сhapter_2 16.11.22

---
+++ JavaBasicCourse/src/chapter_5/EncodeString2.java
@@ -0,0 +1,50 @@
+/**
+ * Using a bitwise exclusive OR operation
+ * to encrypt and decrypt messages
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 11.11.2022
+ */
+
+package chapter_5;
+
+import java.util.Scanner;
+
+public class EncodeString2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст: ");
+        String str = scanner.nextLine();
+        String encode = "";
+        String decode = "";
+        String keyStr = "abcdefgi";
+        int x = 0;
+
+        System.out.print("Исходное сообщение: ");
+        System.out.println(str);
+
+        // шифрование сообщения
+        for (int i = 0; i < str.length(); i++) {
+            encode = encode + (char) (str.charAt(i) ^ keyStr.charAt(x));
+            x++;
+            if (x == 8){
+                x = 0;
+            }
+        }
+        System.out.print("Зашифрованное сообщение: ");
+        System.out.println (encode);
+
+        // дешифровка сообщения
+        x = 0;
+        for (int i = 0; i < str.length (); i++) {
+            // построение дешифрованной строки сообщения
+            decode = decode + (char) (encode.charAt (i) ^ keyStr.charAt (x));
+            x++;
+            if (x == 8){
+                x = 0;
+            }
+        }
+        System.out.print("Дешифрованное сообщение: ");
+        System.out.println (decode);
+    }
+}
