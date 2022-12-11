d351389f81eab7bb0d13fb6c487aee82c9c5ad3f
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-12-11 13:42:11 +0200

    Сhapter_2 11.11.22

---
+++ JavaBasicCourse/src/chapter_6/VarArgs.java
@@ -0,0 +1,29 @@
+/**
+ * Demonstration of the use of the method
+ * with a variable number of arguments
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 08.12.2022
+ */
+
+package chapter_6;
+
+public class VarArgs {
+    //Метод vaTest() допускает переменное число аргументов
+    static void vaTest(int... v) {
+        System.out.println("Koличecтвo аргументов : " + v.length);
+        System.out.println("Coдepжимoe : ");
+
+        for (int i = 0; i < v.length; i++)
+            System.out.println(" arg " + i + " : " + v[i]);
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Метод vaTest() может вызываться с переменным числом аргументов
+        vaTest(10); //1 аргумент
+        vaTest();       // без аргументов
+        vaTest(3, 4, 5); // 3 аргумента
+    }
+}
