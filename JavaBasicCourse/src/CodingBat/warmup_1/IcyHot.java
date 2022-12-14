cd3581f5e930c182b71b7d001c0961780e0cd68b
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-12-15 00:00:16 +0200

    warmup_1 15.12.22

---
+++ JavaBasicCourse/src/CodingBat/warmup_1/IcyHot.java
@@ -0,0 +1,36 @@
+/**
+ * Given two temperatures, return true if
+ * one is less than 0 and the other is greater than 100
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 11.12.2022
+ */
+
+/*
+icyHot(120, -1) → true
+icyHot(-1, 120) → true
+icyHot(2, 120) → false
+*/
+
+package CodingBat.warmup_1;
+
+public class IcyHot {
+    public boolean icyHot(int temp1, int temp2) {
+        return (temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0);
+    }
+}
+
+class IcyHotDemo {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        IcyHot icyHot = new IcyHot();
+
+        System.out.println(icyHot.icyHot(120, -1));
+        System.out.println(icyHot.icyHot(-1, 120));
+        System.out.println(icyHot.icyHot(2, 120));
+    }
+}
---
+++ JavaBasicCourse/src/chapter_6/Main.java
@@ -0,0 +1,14 @@
+package chapter_6;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {1, 2, 3, 4, 5};
+
+        if (arr1 == arr2) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not equal");
+        }
+    }
+}
