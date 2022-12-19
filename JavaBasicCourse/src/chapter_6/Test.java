bbedf6a926bdbd58f29b70910fea1c4a02adcb3e
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-12-19 14:21:14 +0200

    Сhapter_6 19.12.22

--- JavaBasicCourse/src/chapter_6/Test.java
+++ JavaBasicCourse/src/chapter_6/Test.java
@@ -6,22 +6,37 @@
  */
 
 package chapter_6;
-
-public class Test {
+ public class Test {
     int a;
 
     Test(int i) {
         a = i;
     }
 
-    int swap(int s) {
-        return s + a;
+    void swap(Test ob) {
+        ob.a = ob.a + a;
+    }
+
+    void swap(Test st1, Test st2){
+         int x;
+         x = st1.a;
+          st1.a = st2.a;
+          st2.a = x;
     }
 }
 
 class TestDemo {
     public static void main(String[] args) {
         Test t = new Test(5);
-        System.out.println(t.swap(5));
+        Test t2 = new Test(4);
+        System.out.println("t = " + t.a);
+        System.out.println("t2 = " + t2.a);
+        t.swap(t);
+        System.out.println("Go t = " + t.a);
+        t2.swap(t2);
+        System.out.println("Go t2 = " + t2.a);
+        System.out.println("Swap t and t2 \n" + t.a + " and " +  t2.a);
+        t.swap(t,t2);
+        System.out.println("Swap t and t2 \n" + t.a + " and " +  t2.a);
     }
 }
