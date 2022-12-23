c13cd3cf71eb92a7595a5fed203b8f64d46d2fb5
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-12-23 11:12:17 +0200

    warmup_1 23.12.22

---
+++ JavaBasicCourse/src/CodingBat/warmup_1/LoneTeen.java
@@ -0,0 +1,33 @@
+/**
+ * We'll say that a number is "teen" if it is in the range 13..19
+ * inclusive. Given 2 int values, return true if one or the other is teen, but not both.
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 12.12.2022
+ */
+
+/*
+loneTeen(13, 99) → true
+loneTeen(21, 19) → true
+loneTeen(13, 13) → false
+*/
+
+package CodingBat.warmup_1;
+
+public class LoneTeen {
+    public boolean loneTeen(int a, int b) {
+        boolean aa = (a >= 13 && a <= 19);
+        boolean bb = (b >= 13 && b <= 19);
+
+        return (aa && !bb) || (bb && !aa);
+    }
+}
+
+class LoneTeenDemo {
+    public static void main(String[] args) {
+        LoneTeen loneTeen = new LoneTeen();
+
+        System.out.println(loneTeen.loneTeen(13, 99));
+        System.out.println(loneTeen.loneTeen(13, 13));
+    }
+}
