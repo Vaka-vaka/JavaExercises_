d3855b5e1baf896f7e7a19522696384a7171ad02
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2022-12-29 06:28:18 +0200

    chapter_7 29.12.22 warmup_1 29.12.22

---
+++ JavaBasicCourse/src/CodingBat/warmup_1/Close10.java
@@ -0,0 +1,53 @@
+/**
+ * Given 2 int values, return whichever value is nearest
+ * to the value 10, or return 0 in the event of a tie.
+ * Note that Math.abs(n) returns the absolute value of a number.
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 14.12.2022
+ */
+
+/*
+close10(8, 13) → 8
+close10(13, 8) → 8
+close10(13, 7) → 0
+*/
+
+package CodingBat.warmup_1;
+
+public class Close10 {
+    public int close10(int a, int b) {
+        int aa = Math.abs(a - 10);
+        int bb = Math.abs(b - 10);
+
+//        int a1 = Math.abs(aa);
+//        int b1 = Math.abs(bb);
+
+        if (aa == bb) return 0;
+
+        /*
+        if(a1 < b1){
+        return a;
+        }else{
+        return b;
+        }
+        */
+
+        return Math.min(a, b);
+    }
+}
+
+class Close10Demo {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        Close10 close10 = new Close10();
+
+        System.out.println(close10.close10(8, 13));
+        System.out.println(close10.close10(13, 8));
+        System.out.println(close10.close10(13, 7));
+    }
+}
---
+++ JavaBasicCourse/src/chapter_7/A3.java
@@ -0,0 +1,51 @@
+/**
+ * Переопределение метода
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 26.12.2022
+ */
+
+package chapter_7;
+
+public class A3 {
+    int i, j;
+
+    A3(int a, int b) {
+        i = a;
+        j = b;
+    }
+
+    //Отображение переменных i и j
+    void show() {
+        System.out.println("i and j " + i + " " + j);
+    }
+}
+
+class B3 extends A3 {
+    int k;
+
+    B3(int a, int b, int c) {
+        super(a, b);
+
+        k = c;
+    }
+
+    //     Отображение переменной k - переопределение метода show() в А2
+    void show1() {
+        super.show();
+        System.out.println("k " + k);
+    }
+
+    void show2(String msg) {
+        System.out.println(msg + k);
+    }
+}
+
+class Override2 {
+    public static void main(String[] args) {
+        B3 a2 = new B3(1, 2, 3);
+        a2.show(); //вызов метода show() из класса A3
+        a2.show1(); //вызов метода show() из класса В3
+        a2.show2("k - "); //вызов метода show() из класса В3
+    }
+}
