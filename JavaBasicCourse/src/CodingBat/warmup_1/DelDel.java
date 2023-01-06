40349822331ff3e33cad8b0fc24a5653dcb44d40
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-01-06 16:07:05 +0200

    warmup_1 06.01.23

---
+++ JavaBasicCourse/src/CodingBat/warmup_1/DelDel.java
@@ -0,0 +1,34 @@
+/**
+ * Given a string, if the string "del" appears starting at index 1,
+ * return a string where that "del" has been deleted. Otherwise, return the string unchanged
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 12.12.2022
+ */
+
+/*
+delDel("adelbc") → "abc"
+delDel("adelHello") → "aHello"
+delDel("adedbc") → "adedbc"
+*/
+
+package CodingBat.warmup_1;
+
+public class DelDel {
+    public String delDel(String str) {
+        if (str.length() > 4 && str.substring(1, 4).equals("del")) {
+            return str.substring(0, 1) + str.substring(4);
+        }
+        return str;
+    }
+}
+
+class DelDelDemo {
+    public static void main(String[] args) {
+        DelDel delDel = new DelDel();
+
+        System.out.println(delDel.delDel("adelbc"));
+        System.out.println(delDel.delDel("adelHello"));
+
+    }
+}
