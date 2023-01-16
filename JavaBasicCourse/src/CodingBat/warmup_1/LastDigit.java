5ba6af7763998fb19cde10437f42807d025e7634
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-01-16 15:15:09 +0200

    warmup_1 16.01.23

---
+++ JavaBasicCourse/src/CodingBat/warmup_1/LastDigit.java
@@ -0,0 +1,32 @@
+/**
+ * lastDigit
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 29.12.2022
+ */
+
+/*
+lastDigit(7, 17) → true
+lastDigit(6, 17) → false
+lastDigit(3, 113) → true
+*/
+
+package CodingBat.warmup_1;
+
+public class LastDigit {
+    boolean lastDigit(int a, int b) {
+//        int a1 = (a % 10);
+//        int b1 = (b % 10);
+//        System.out.println(a1 + " " + b1);
+        return (a%10 == b%10);
+    }
+}
+
+class LastDigitDemo {
+    public static void main(String[] args) {
+        LastDigit lastDigit = new LastDigit();
+        System.out.println(lastDigit.lastDigit(7, 17));
+        System.out.println(lastDigit.lastDigit(6, 17));
+        System.out.println(lastDigit.lastDigit(3, 113));
+    }
+}
