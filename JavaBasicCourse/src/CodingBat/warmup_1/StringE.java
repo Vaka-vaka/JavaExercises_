b15bc99f458ffc85914d46486b461acc50bf4953
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-01-18 14:41:22 +0200

    warmup_1 18.01.23

---
+++ JavaBasicCourse/src/CodingBat/warmup_1/StringE.java
@@ -0,0 +1,35 @@
+/**
+ * stringE
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 28.12.2022
+ */
+
+/*
+stringE("Hello") → true
+stringE("Heelle") → true
+stringE("Heelele") → false
+*/
+
+package CodingBat.warmup_1;
+
+public class StringE {
+    boolean stringE(String str){
+       int count = 0, count2 = 0;
+       for(int i = 0; i < str.length(); i++){
+           if(str.charAt(i) == 'e') count++;
+           if(str.substring(i).equals("l")) count2++;
+       }
+        System.out.println(count);
+        System.out.println(count2);
+       return ((count >= 1 && count <= 3) && (count2 >= 1 && count2 <= 3));
+    }
+}
+
+class StringEDemo{
+    public static void main(String[] args) {
+        StringE stringE = new StringE();
+        System.out.println(stringE.stringE("Hellell"));
+        System.out.println(stringE.stringE("Helleeell"));
+    }
+}
