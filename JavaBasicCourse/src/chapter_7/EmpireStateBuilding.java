91f8cb1cf234e8b43e01e30b31fdf79bbb7745d3
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-02-01 16:24:00 +0200

    chapter_7 01.02.23

---
+++ JavaBasicCourse/src/chapter_7/EmpireStateBuilding.java
@@ -0,0 +1,40 @@
+/**
+ * Ліфт Empire State Building
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 01.02.2023
+ */
+
+package chapter_7;
+
+import java.util.Scanner;
+
+public class EmpireStateBuilding {
+    public static void main(String[] args) {
+        DemoLogin demoLogin = new DemoLogin();
+
+        System.out.println("Який поверх!");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number < 1 || number > 102){
+            System.out.println("невірний поверх");
+        }else if(number > 39 && number < 102 || number < 24 && number > 0){
+            System.out.println("ok");
+        } else if (number >= 24 && number <= 39) {
+            demoLogin.login();
+        }
+    }
+}
+
+class DemoLogin{
+    void login(){
+        System.out.println("Введіть логін");
+        Scanner scanner = new Scanner(System.in);
+        int login1 = scanner.nextInt();
+        if(login1 == 1337){
+            System.out.println("ok");
+        }else {
+            System.out.println("невірний пароль");
+        }
+    }
+}
