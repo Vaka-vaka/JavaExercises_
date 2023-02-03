7a2425430148e617bace9d5df43784e39fee6e1f
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-02-03 16:08:41 +0200

    chapter_7 03.02.23

--- JavaBasicCourse/src/chapter_7/Test.java
+++ JavaBasicCourse/src/chapter_7/Test.java
@@ -8,15 +8,36 @@ package chapter_7;
 
 public class Test {
 
-
     public static void main(String[] args) {
-        int res = 0;
-        int count = 3;
+//        int res = 0;
+//        int count = 3;
+//
+//        do {
+//            count += 1;
+//            res += count;
+//        }while (count < 3);
+//        System.out.println(res);
 
-        do {
-            count += 1;
-            res += count;
-        }while (count < 3);
-        System.out.println(res);
+
+//        int a = 3;
+//        int i;
+//        for (i = 2; i <= a; i++) {
+//            if (i % 2 == 0) {
+//                System.out.print(i);
+//                if (i < (a - 1)) {
+//                    System.out.print(",");
+//                }
+//            }
+//        }
+
+        int t = 5;
+        for (int q = 0; q < t; q++) {
+            int x = 0;
+            while (x < t) {
+                System.out.print("*");
+                x++;
+            }
+            System.out.println();
+        }
     }
 }
