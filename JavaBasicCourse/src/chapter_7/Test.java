6cdf384acded8f6d0e13544a8507da1d39b0db94
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-02-04 00:03:21 +0200

    chapter_7 04.02.23

--- JavaBasicCourse/src/chapter_7/Test.java
+++ JavaBasicCourse/src/chapter_7/Test.java
@@ -6,6 +6,8 @@
 
 package chapter_7;
 
+import java.util.Scanner;
+
 public class Test {
 
     public static void main(String[] args) {
@@ -30,13 +32,58 @@ public class Test {
 //            }
 //        }
 
-        int t = 5;
-        for (int q = 0; q < t; q++) {
-            int x = 0;
-            while (x < t) {
-                System.out.print("*");
-                x++;
-            }
+//        int t = 5;
+//        for (int q = 0; q < t; q++) {
+//            int x = 0;
+//            while (x < t) {
+//                System.out.print("*");
+//                x++;
+//            }
+//            System.out.println();
+//        }
+//
+//        int a = 4;
+//        for (int i = 0; i < a; i++) {
+//            int y = a - i;
+//            for(int x = 0; x < y; x++)
+//                System.out.print("*");
+//                System.out.println();
+//        }
+
+//        System.out.println("Введіть розмір трикутника");
+//        Scanner scanner = new Scanner(System.in);
+//        int size = scanner.nextInt();
+//        for(int i = size; i > 0; i--){
+//            for(int y = 0; y < i; y++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println("Введіть розмір трикутника");
+//        Scanner scanner = new Scanner(System.in);
+//        int size = scanner.nextInt();
+//
+//        int i;
+//        i = size;
+//        while (i > 0) {
+//            int y;
+//            y = 0;
+//            while (y < i){
+//                System.out.print("*");
+//                y++;
+//            }
+//            System.out.println();
+//            i--;
+//        }
+
+        System.out.println("Введіть розмір трикутника");
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+
+        for(int i = 1; i <= size; i++){
+           for (int y = 0; y < i; y++){
+               System.out.print("*");
+           }
             System.out.println();
         }
     }
