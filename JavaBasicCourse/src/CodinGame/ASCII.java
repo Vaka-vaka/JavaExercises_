e91364e8a1cb0e93ffccfe8549316726a3923042
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-02-11 21:50:48 +0200

    CodinGame 11.02.23

---
+++ JavaBasicCourse/src/CodinGame/ASCII.java
@@ -0,0 +1,19 @@
+/**
+ * You are given a single string.
+ * You need to convert each character into it's ASCII
+ * value and add the odd numbers together.
+ *
+ * Example
+ * Input: ABC
+ * A,B,C → 65,66,67
+ * Take odd numbers
+ * 65+67=132
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 08.02.2023
+ */
+
+package CodinGame;
+
+public class ASCII {
+}
