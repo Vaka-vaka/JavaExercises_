d6756807fad74cf5dd8c0514f1d90c08a73ef059
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-02-13 16:42:41 +0200

    CodinGame 13.02.23

---
+++ JavaBasicCourse/src/CodinGame/TheDart101.java
@@ -0,0 +1,37 @@
+/**
+ * The Dart 101
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 09.02.2023
+ */
+
+package CodinGame;
+
+import java.util.Scanner;
+
+public class TheDart101 {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+        for (int i = 0; i < N; i++) {
+            String PLAYER = in.nextLine();
+        }
+        for (int i = 0; i < N; i++) {
+            String SHOOTS = in.nextLine();
+        }
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("winner");
+
+    }
+}
