c2cafdf732bae4571deae4a8e15691a85a49a2c0
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-02-20 16:32:24 +0200

    CodinGame 20.02.23

---
+++ JavaBasicCourse/src/chapter_8/IcharQ/IChar_Q.java
@@ -0,0 +1,16 @@
+/**
+ * Interface for character queue
+ * Интерфейс для очереди символов
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 20.02.2023
+ */
+
+package chapter_8.IcharQ;
+
+public interface IChar_Q {
+    // Помещение символа в очередь
+    void put(char ch);
+    //Извлечение символа из очереди
+    char get();
+}
