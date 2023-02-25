584295b96899ba3d305a43b40a9b09bbc42529df
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-02-25 22:26:32 +0200

    chapter_8 26.02.23

--- JavaBasicCourse/src/chapter_8/IQDemo/CircularQueue.java
+++ JavaBasicCourse/src/chapter_8/IQDemo/CircularQueue.java
@@ -23,11 +23,26 @@ public class CircularQueue implements IChar_Q {
     // Помещение символа в очередь
     @Override
     public void put(char ch) {
-
+        // Очередь считается полной, если индекс putloc на единицу
+        // меньше индекса getloc или если индекс putloc указывает
+        // на конец массива, а индекс getloc - на его начало
+        if(putloc + 1 == getloc | (putloc == q.length - 1 && getloc == 0)){
+            System.out.println(" - Очередь заполнена");
+            return;
+        }
+        q[putloc ++] = ch;
+        if(putloc == q.length) putloc = 0;
     }
 
+    //Извлечение символа из очереди
     @Override
     public char get() {
-        return 0;
+       if(putloc == getloc){
+           System.out.println(" - Очередь пуста");
+           return (char) 0;
+       }
+       char ch = q[getloc++];
+       if(getloc == q.length) getloc = 0; //вернуться в начало очереди
+        return q[getloc];
     }
 }
---
+++ JavaBasicCourse/src/chapter_8/IQDemo/DynQueue.java
@@ -0,0 +1,23 @@
+/**
+ * Dynamic queue
+ * Динамическая очередь
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 26.02.2023
+ */
+
+package chapter_8.IQDemo;
+
+import chapter_8.IcharQ.IChar_Q;
+
+public class DynQueue implements IChar_Q {
+    @Override
+    public void put(char ch) {
+
+    }
+
+    @Override
+    public char get() {
+        return 0;
+    }
+}
