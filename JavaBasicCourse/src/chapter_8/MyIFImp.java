4af4b45665b1421938bdbf3768874fe34ba00652
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-03-07 16:04:27 +0200

    chapter_8 07.03.23

--- JavaBasicCourse/src/chapter_8/MyIFImp.java
+++ JavaBasicCourse/src/chapter_8/MyIFImp.java
@@ -8,7 +8,7 @@
 
 package chapter_8;
 
-public class MyIFImp implements MyIF{
+public class MyIFImp implements MyIF {
     //Реализации подлежит лишь метод getUserID() интерфейса MyIF.
     // Делать это для метода getAdminID() необязательно, поскольку
     // при необходимости может быть использована его реализация, заданная по умолчанию.
