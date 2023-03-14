5395585dec811a04e085ed8cc663352cddb7b8cf
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-03-14 15:42:29 +0200

    chapter_9 14.03.23

---
+++ JavaBasicCourse/src/chapter_9/ExcDemo.java
@@ -0,0 +1,30 @@
+/**
+ * Demonstration of exception handling
+ * Демонстрация обработки исключений
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 14.03.2023
+ */
+
+package chapter_9;
+
+public class ExcDemo {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        int[] nums = new int[4];
+
+        try { //Создание блока try
+            System.out.println("Дo генерации исключения");
+
+            nums[7] = 10; //Попытка выйти за пределы массива nums
+            System.out.println("Этa строка не будет отображаться");
+        } catch (ArrayIndexOutOfBoundsException exc) { //Перехват ошибок, вызываемых выходом за пределы массива
+            System.out.println("Bыxoд за пределы массива!");
+        }
+        System.out.println("Пocлe инструкции catch");
+    }
+}
