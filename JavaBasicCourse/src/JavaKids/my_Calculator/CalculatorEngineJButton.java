6e3bf2e21b2803850b53b579168471cf22cbf68b
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-04-06 00:10:18 +0300

    tasks 06.04.23

--- JavaBasicCourse/src/JavaKids/my_Calculator/CalculatorEngineJButton.java
+++ JavaBasicCourse/src/JavaKids/my_Calculator/CalculatorEngineJButton.java
@@ -19,22 +19,79 @@ public class CalculatorEngineJButton implements ActionListener {
 
     // Конструктор сохраняет ссылку на окно калькулятора
     // в переменной экземпляра класса
-    CalculatorEngineJButton(Calculator parent){
+    CalculatorEngineJButton(Calculator parent) {
         this.parent = parent;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        //Получаем источник события JButton
+        // Получить источник действия
         JButton clickedJButton = (JButton) e.getSource();
+        String dispFieldText = parent.displayField.getText();
 
-        //Получаем надпись на кнопке JButton
-        String clickedJButtonLabel = clickedJButton.getText();
+        double displayValue = 0;
 
-        // Добавляем надпись на кнопке к тексту окна сообщения
-        JOptionPane.showConfirmDialog(null,
-                "You pressed " + clickedJButtonLabel,
-                "Just a test",
-                JOptionPane.PLAIN_MESSAGE);
+        // Получить число из дисплея калькулятора,
+        // если он не пустой.
+        // Восклицательный знак – это оператор отрицания
+        if (!"".equals(dispFieldText)) {
+            displayValue = Double.parseDouble(dispFieldText);
+        }
+
+
+        Object src = e.getSource();
+        // Для каждой кнопки арифметического действия
+        // запомнить его: +, -, /, или *, сохранить текущее число
+        // в переменной currentResult, и очистить дисплей
+        // для ввода нового числа
+        if (src == parent.buttonPlus) {
+            selectedAction = '+';
+            currentResult = displayValue;
+            parent.displayField.setText("");
+        } else if (src == parent.buttonMinus) {
+            selectedAction = '-';
+            currentResult = displayValue;
+            parent.displayField.setText("");
+        } else if (src == parent.buttonDivision) {
+            selectedAction = '/';
+            if (dispFieldText.equals("")){
+                JOptionPane.showConfirmDialog(null,
+                        "На ноль делить нельзя",
+                        "Just a test",
+                        JOptionPane.PLAIN_MESSAGE);
+                System.out.println("На ноль делить нельзя");
+            } else {
+                currentResult = displayValue;
+                parent.displayField.setText("");
+            }
+        } else if (src == parent.buttonMultiplication) {
+            selectedAction = '*';
+            currentResult = displayValue;
+            parent.displayField.setText("");
+        } else if (src == parent.buttonEquals) {
+            // Совершить арифметическое действие, в зависимости
+            // от selectedAction, обновить переменную currentResult
+            // и показать результат на дисплее
+            if (selectedAction == '+') {
+                currentResult += displayValue;
+                // Сконвертировать результат в строку, добавляя его
+                // к пустой строке и показать его
+                parent.displayField.setText("" + currentResult);
+            } else if (selectedAction == '-') {
+                currentResult -= displayValue;
+                parent.displayField.setText("" + currentResult);
+            } else if (selectedAction == '/') {
+                    currentResult /= displayValue;
+                    parent.displayField.setText("" + currentResult);
+            } else if (selectedAction == '*') {
+                currentResult *= displayValue;
+                parent.displayField.setText("" + currentResult);
+            }
+        } else {
+            // Для всех цифровых кнопок присоединить надпись на
+            // кнопке к надписи в дисплее
+            String clickedJButtonLabel = clickedJButton.getText();
+            parent.displayField.setText(dispFieldText + clickedJButtonLabel);
+        }
     }
 }
