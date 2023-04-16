ca2789153daeeb0f0b4bf71feeb3488e6524dbd3
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-04-16 19:23:29 +0300

    tasks 16.04.23

--- JavaBasicCourse/src/tasks/Car.java
+++ JavaBasicCourse/src/tasks/Car.java
@@ -1,4 +1,18 @@
 package tasks;
 
 public class Car {
+    static int x = 10;
+    public static void main(String[] args) {
+        for(int x = 5; x < 5; x++) {
+        }
+        System.out.print(x);
+    }
+
 }
+
+//class sdfsdf{
+//    public static void main(String[] args) {
+//        Car car = new Car();
+//        System.out.println();
+//    }
+//}
