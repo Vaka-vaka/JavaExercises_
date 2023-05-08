0ce478eade51fe25527373fd026306b53bdb4d46
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-05-08 14:43:42 +0300

    swing 08.05.23

---
+++ JavaBasicCourse/src/JavaKids/swing/Lesson5.java
@@ -0,0 +1,81 @@
+/**
+ * swing WindowListener
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 04.05.2023
+ */
+
+package JavaKids.swing;
+
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class Lesson5 {
+    public static void main(String[] args) throws Exception {
+        JFrame jFrame = getFrame();
+        jFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                super.windowOpened(e);
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+                super.windowClosed(e);
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+                super.windowIconified(e);
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+                super.windowDeiconified(e);
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+                super.windowActivated(e);
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+                super.windowDeactivated(e);
+            }
+
+            @Override
+            public void windowStateChanged(WindowEvent e) {
+                super.windowStateChanged(e);
+            }
+
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                super.windowGainedFocus(e);
+            }
+
+            @Override
+            public void windowLostFocus(WindowEvent e) {
+                super.windowLostFocus(e);
+            }
+        });
+
+
+    }
+
+    static JFrame getFrame() {
+        JFrame jFrame = new JFrame() {
+        };
+        jFrame.setVisible(true);
+        jFrame.setBounds(750, 250, 500, 500);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        return jFrame;
+    }
+}
