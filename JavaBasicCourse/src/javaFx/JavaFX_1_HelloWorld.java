7db4865530a08f8489369faa9939d49cb57da1b1
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-05-24 16:40:12 +0300

    javaFx 24.05.23

---
+++ JavaBasicCourse/src/javaFx/JavaFX_1_HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * Java FX: HelloWorld
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 24.05.2023
+ */
+
+package javaFx;
+//import javafx.application.Application;
+
+public class JavaFX_1_HelloWorld {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
