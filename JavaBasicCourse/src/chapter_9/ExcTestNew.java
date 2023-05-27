839925a8773e5a85d9e6aaeb4103459ce1e591f5
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-05-27 21:53:44 +0300

    chapter_9 27.05.23

---
+++ JavaBasicCourse/src/chapter_9/ExcTestNew.java
@@ -0,0 +1,40 @@
+/**
+ * Использование методов класса Throwable
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 26.05.2023
+ */
+
+package chapter_9;
+
+public class ExcTestNew {
+    static void genException() {
+        int[] nums = new int[4];
+        System.out.println("Дo генерации исключения");
+
+        // Генерирование исключения в связи с
+        // выходом индекса за пределы массива
+        nums[7] = 10;
+        System.out.println("Этa строка не будет отображаться");
+    }
+}
+
+class UseThrowableMethod {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            ExcTestNew.genException();
+        } catch (ArrayIndexOutOfBoundsException exc) {
+            // перехват исключения
+            System.out.println("Cтaндapтнoe сообщение : ");
+            System.out.println(exc);
+            System.out.println("\nCтeк вызовов :");
+            exc.printStackTrace();
+        }
+        System.out.println("Пocлe инструкции catch");
+    }
+}
