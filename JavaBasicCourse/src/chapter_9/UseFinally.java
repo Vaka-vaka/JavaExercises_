90998c5fb1ad10364d7dcf48f8506c5d3c153ddf
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-05-29 16:00:55 +0300

    chapter_9 29.05.23

---
+++ JavaBasicCourse/src/chapter_9/UseFinally.java
@@ -0,0 +1,50 @@
+/**
+ * Использование блока finally
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 26.05.2023
+ */
+
+package chapter_9;
+
+public class UseFinally {
+    public static void genException(int what) {
+        int t;
+        int[] nums = new int[2];
+        System.out.println("Пoлyчeнo : " + what);
+        try {
+            switch (what) {
+                case 0:
+                    t = 10 / what; // сгенерировать ошибку деления на нуль
+                    break;
+                case 1:
+                    nums[4] = 4; // сгенерировать ошибку обращения к массиву
+                    break;
+                case 2:
+                    // возврат из блока try
+
+            }
+        } catch (ArithmeticException exc) {
+            System.out.println("Пoпыткa деления на нуль");
+            // возврат из блока catch
+        } catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("Cooтвeтcтвyющий элемент не найден");
+        } finally {
+            System.out.println("Bыxoд из блока try");
+        }
+    }
+}
+
+class FinallyDemo {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            UseFinally.genException(i);
+            System.out.println();
+        }
+    }
+}
