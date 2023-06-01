694f139346717d1266d4162e72dc77372f4622a0
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-06-01 15:06:34 +0300

    chapter_9 01.06.23

---
+++ JavaBasicCourse/src/chapter_9/ThrowsDemo.java
@@ -0,0 +1,31 @@
+/**
+ * Использование ключевого слова throws
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 01.06.2023
+ */
+
+package chapter_9;
+
+import java.util.jar.JarException;
+
+public class ThrowsDemo {
+    public static char prompt(String str)
+            throws java.io.IOException {
+        System.out.println(str + ": ");
+        return (char) System.in.read();
+    }
+
+    public static void main(String[] args) {
+        char ch;
+
+        try {
+            ch = prompt("Введите букву ");
+        } catch (java.io.IOException exc) {
+            System.out.println("Пpoизoшлo исключение ввода- вывода ");
+
+            ch = 'X';
+        }
+        System.out.println("Bы нажали клавишу " + ch);
+    }
+}
