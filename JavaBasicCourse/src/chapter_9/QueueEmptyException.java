d8437cb451f0d6063283ae069d65e825b48e5973
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-06-12 22:29:03 +0300

    chapter_9 12.06.23

---
+++ JavaBasicCourse/src/chapter_9/QueueEmptyException.java
@@ -0,0 +1,17 @@
+/**
+ * Использование специально создаваемого исключения
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 09.06.2023
+ */
+
+package chapter_9;
+
+//Исключение для ошибок, связаннь� с пустой очередью
+public class QueueEmptyException extends Exception {
+    int empty;
+
+    public String toString() {
+        return "\nОчередь пуста.";
+    }
+}
