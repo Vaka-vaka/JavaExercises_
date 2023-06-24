e2161a0a9f6604a8c366451e583ee7e43272b46c
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-06-24 22:45:38 +0300

    Java_ChanJamie.chapter_7 24.06.23

---
+++ JavaBasicCourse/src/Java_ChanJamie/chapter_7/Staff.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 24.06.2023
+ */
+package Java_ChanJamie.chapter_7;
+
+public class Staff {
+    private String nameOfStaff; //имя сотрудника
+    private final int hourlyRate = 30; //почасовая ставка
+    private int hoursWorked; //отработанные часы
+
+
+
+
+
+}
