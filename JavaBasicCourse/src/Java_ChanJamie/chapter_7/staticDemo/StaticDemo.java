0586c625729a6ef386526255e38067c2717d0e18
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-07-21 21:37:56 +0300

    Java_ChanJamie.chapter_7 21.07.23

---
+++ JavaBasicCourse/src/Java_ChanJamie/chapter_7/staticDemo/StaticDemo.java
@@ -0,0 +1,26 @@
+/**
+ * static class demo
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 13.07.2023
+ */
+
+package Java_ChanJamie.chapter_7.staticDemo;
+
+public class StaticDemo {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        MyClass sd = new MyClass();
+        System.out.println(sd.getMessage());
+        sd.setMessage("food");
+        sd.displayMessage();
+
+
+        System.out.println(MyClass.getGreetings());
+        MyClass.displayGreetings();
+    }
+}
