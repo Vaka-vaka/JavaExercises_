fcdc1e573b75e36cc9b7bc32967e35739de6e114
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-07-24 23:05:41 +0300

    Java_ChanJamie.chapter_7 24.07.23

---
+++ JavaBasicCourse/src/Java_ChanJamie/chapter_7/inheritanceDemo/NormalMember.java
@@ -0,0 +1,25 @@
+/**
+ * demo inheritance
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 21.07.2023
+ */
+
+package Java_ChanJamie.chapter_7.inheritanceDemo;
+
+public class NormalMember extends Member {
+
+    public NormalMember() {
+        System.out.println("Child constructor with no parameter");
+    }
+
+    public NormalMember(String pName, int pMemberID, int pMemberSince) {
+        super(pName, pMemberID, pMemberSince);
+        System.out.println("Child Constructor with 3 parameters");
+    }
+
+    @Override
+    public void calculateAnnualFee() {
+        annualFee = (1 - 0.01 * getDiscount() * (100 + 12 * 30));
+    }
+}
