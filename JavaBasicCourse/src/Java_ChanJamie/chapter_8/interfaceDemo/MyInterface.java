4075c01fa4d885b6b19e110fa13acb973ecf412a
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-07-27 22:20:52 +0300

    Java_ChanJamie.chapter_8 27.07.23

---
+++ JavaBasicCourse/src/Java_ChanJamie/chapter_8/interfaceDemo/MyInterface.java
@@ -0,0 +1,22 @@
+/**
+ * demo interface
+ *
+ * @author Valentyn Mozul
+ * @version 1.0 from 25.07.2023
+ */
+
+package Java_ChanJamie.chapter_8.interfaceDemo;
+
+public interface MyInterface {
+    int myInt = 5;
+
+    void someMethod();
+
+    public static void someStaticMethod() {
+        System.out.println("This is a static method in an interface");
+    }
+
+    public default void someDefoultMethod() {
+        System.out.println("This is a default method in an interface");
+    }
+}
