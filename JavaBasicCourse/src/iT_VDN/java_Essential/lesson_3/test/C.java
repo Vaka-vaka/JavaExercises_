d3a03658d0c07a30d0fe8fa00f16acbb71581241
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-10-04 21:48:41 +0300

    java_Essential 04.10.23

---
+++ JavaBasicCourse/src/iT_VDN/java_Essential/lesson_3/test/C.java
@@ -0,0 +1,14 @@
+package iT_VDN.java_Essential.lesson_3.test;
+
+class C {
+    protected void method() {
+
+    }
+}
+
+class Z extends C {
+    @Override
+    protected void method() {
+        super.method();
+    }
+}
