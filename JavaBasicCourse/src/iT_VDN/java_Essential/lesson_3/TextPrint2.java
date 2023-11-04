5ae7d7b35b7ea0a4c8094772a59395f5fd8b690e
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-11-04 18:15:49 +0200

    java_Essential 04.11.23

---
+++ JavaBasicCourse/src/iT_VDN/java_Essential/lesson_3/TextPrint2.java
@@ -0,0 +1,13 @@
+/**
+ * java basic
+ * lesson 3
+ *
+ * @version 1.0 from 2023-09-07
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential.lesson_3;
+
+public class TextPrint2 extends Printer {
+    public String value;
+}
