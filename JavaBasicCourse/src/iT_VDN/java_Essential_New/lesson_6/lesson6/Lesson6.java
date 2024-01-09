7d0dd0a933f440db79b83335529732d97665fe54
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-01-09 19:37:07 +0200

    java_Essential 09.01.24

---
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_6/lesson6/Lesson6.java
@@ -0,0 +1,25 @@
+/**
+ * java basic
+ * lesson 6
+ *
+ * @version 1.0 from 2023-12-18
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential_New.lesson_6.lesson6;
+
+public class Lesson6 {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        ListExamples examples = new ListExamples();
+        //examples.runListExample1();
+        SetExamples setExamples = new SetExamples();
+      //  setExamples.runSetExamples();
+        MapExamples examples1 = new MapExamples();
+        examples1.runMapExamples();
+    }
+}
