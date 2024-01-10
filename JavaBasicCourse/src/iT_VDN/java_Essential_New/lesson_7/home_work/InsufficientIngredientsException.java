084d2ef24dec283632654c4d2f803dc2cddd112f
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-01-10 16:21:09 +0200

    java_Essential 10.01.24

---
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_7/home_work/InsufficientIngredientsException.java
@@ -0,0 +1,16 @@
+/**
+ * java basic
+ * lesson 7
+ *
+ * @version 1.0 from 2024-01-01
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential_New.lesson_7.home_work;
+
+public class InsufficientIngredientsException extends Exception {
+
+    public InsufficientIngredientsException(String message) {
+        super(message);
+    }
+}
