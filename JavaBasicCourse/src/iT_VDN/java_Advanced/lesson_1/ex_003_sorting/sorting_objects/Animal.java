5b8868afb969543eb6c9fc85ff5d3ff5954bec9d
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-01-31 12:16:11 +0200

    java_Essential 31.01.24

---
+++ JavaBasicCourse/src/iT_VDN/java_Advanced/lesson_1/ex_003_sorting/sorting_objects/Animal.java
@@ -0,0 +1,23 @@
+/**
+ * java advanced
+ * lesson 1
+ *
+ * @version 1.0 from 2023-11-20
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Advanced.lesson_1.ex_003_sorting.sorting_objects;
+
+public class Animal {
+    String breed;
+    int weight;
+    int speed;
+    int price;
+
+    public Animal(String breed, int weight, int speed, int price) {
+        this.breed = breed;
+        this.weight = weight;
+        this.speed = speed;
+        this.price = price;
+    }
+}
