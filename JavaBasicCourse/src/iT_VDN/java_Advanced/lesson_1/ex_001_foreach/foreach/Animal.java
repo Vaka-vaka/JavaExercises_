0a2859fc3fe65a3337c0fd8af83e9c60859595a4
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-02-06 15:48:02 +0200

    java_Essential 06.02.24

---
+++ JavaBasicCourse/src/iT_VDN/java_Advanced/lesson_1/ex_001_foreach/foreach/Animal.java
@@ -0,0 +1,19 @@
+/**
+ * java advanced
+ * lesson 1
+ *
+ * @version 1.0 from 2023-11-16
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Advanced.lesson_1.ex_001_foreach.foreach;
+
+public class Animal {
+    String breed;
+    int price;
+
+    public Animal(String breed, int price) {
+        this.breed = breed;
+        this.price = price;
+    }
+}
