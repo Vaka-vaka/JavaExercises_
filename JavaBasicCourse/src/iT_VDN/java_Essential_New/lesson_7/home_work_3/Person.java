a8b5ddbc6d7f2b30469c0f3c8d64e9d6e80e16af
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-02-07 14:00:52 +0200

    java_Essential 07.02.24

---
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_7/home_work_3/Person.java
@@ -0,0 +1,35 @@
+/**
+ * java basic
+ * lesson 7
+ *
+ * @version 1.0 from 2024-02-06
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential_New.lesson_7.home_work_3;
+
+public class Person {
+    private String name;
+
+    Person() {
+    }
+
+    Person(String name) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void introduce() {
+        System.out.println("Hello!" + " " + getName() + " " + "How are you?");
+    }
+
+    public void introducePerson(Person person) {
+        person.introduce();
+    }
+}
