06b5244093cccf4ca0bd2138d9ad4bfb0c938c9c
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-02-19 15:55:16 +0200

    java_Essential 19.02.24

---
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_7/home_work_3/ZeroDivisionException.java
@@ -0,0 +1,15 @@
+/**
+ * java basic
+ * lesson 7
+ *
+ * @version 1.0 from 2024-02-15
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential_New.lesson_7.home_work_3;
+
+public class ZeroDivisionException extends Exception {
+    ZeroDivisionException (String message) {
+        super(message);
+    }
+}
