780df98b5207e28e6df08ae212dbcf996c3e62b8
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-02-29 16:20:16 +0200

    java_Essential 29.02.24

---
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_7/home_work_3/DateProcessor.java
@@ -0,0 +1,17 @@
+/**
+ * java basic
+ * lesson 7
+ *
+ * @version 1.0 from 2024-02-19
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential_New.lesson_7.home_work_3;
+
+public class DateProcessor {
+    public void processData(int num) throws ZeroDivisionException {
+        if (num == 0) {
+            throw new ZeroDivisionException("num is null!!!");
+        }
+    }
+}
