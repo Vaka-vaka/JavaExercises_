3d808f3c102182ef874dc8063c6176f8d7b214f9
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-03-04 16:26:36 +0200

    java_Essential 4.03.24

---
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_7/home_work_3/NegativeNumberException.java
@@ -0,0 +1,16 @@
+/**
+ * java basic
+ * lesson 7
+ *
+ * @version 1.0 from 2024-02-15
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential_New.lesson_7.home_work_3;
+
+public class NegativeNumberException extends Exception {
+
+    NegativeNumberException(String message) {
+        super(message);
+    }
+}
