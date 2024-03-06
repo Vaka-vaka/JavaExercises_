27887a8dbfa3526f84d9ebd933f4c226f8c7f593
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-03-06 22:41:03 +0200

    java_Essential 06.03.24

---
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_8/lesson8/Lesson8.java
@@ -0,0 +1,13 @@
+/**
+ * java basic
+ * lesson 8
+ *
+ * @version 1.0 from 2024-03-05
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential_New.lesson_8.lesson8;
+
+public class Lesson8 {
+
+}
