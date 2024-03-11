d24439e6c86495459df0b187d4e129410bae379b
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-03-11 16:33:02 +0200

    todo_list 11.03.24

--- JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_8/lesson8/Lesson8.java
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_8/lesson8/Lesson8.java
@@ -2,12 +2,19 @@
  * java basic
  * lesson 8
  *
- * @version 1.0 from 2024-03-05
+ * @version 1.0 from 2024-03-11
  * @author Valentyn Mozul
  */
 
 package iT_VDN.java_Essential_New.lesson_8.lesson8;
 
 public class Lesson8 {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
 
+    }
 }
