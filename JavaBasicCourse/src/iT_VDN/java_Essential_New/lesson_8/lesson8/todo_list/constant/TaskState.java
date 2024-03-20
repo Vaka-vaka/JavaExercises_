44eff5a232c9aea720e708ed016205fbfdcb2bc8
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-03-20 16:19:21 +0200

    todo_list 20.03.24

---
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_8/lesson8/todo_list/constant/TaskState.java
@@ -0,0 +1,15 @@
+/**
+ * java basic
+ * lesson 8
+ *
+ * @version 1.0 from 2024-03-12
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential_New.lesson_8.lesson8.todo_list.constant;
+
+public enum TaskState {
+    TODO,
+    IN_PROGRESS,
+    COMPLETE
+}
