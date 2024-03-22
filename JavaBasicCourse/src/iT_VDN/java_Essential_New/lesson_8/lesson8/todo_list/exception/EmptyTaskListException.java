3d111d4c427ef00d05ead1c51d43839cc6cc4765
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-03-22 14:55:18 +0200

    todo_list 22.03.24

---
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_8/lesson8/todo_list/exception/EmptyTaskListException.java
@@ -0,0 +1,16 @@
+/**
+ * java basic
+ * lesson 8
+ *
+ * @version 1.0 from 2024-03-21
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential_New.lesson_8.lesson8.todo_list.exception;
+
+public class EmptyTaskListException extends RuntimeException {
+
+    public EmptyTaskListException(String message) {
+        super(message);
+    }
+}
