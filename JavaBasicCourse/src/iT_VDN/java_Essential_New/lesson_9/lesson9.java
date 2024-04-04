a224013ada9086f3a7d9656decc1278458deba08
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-04-04 14:58:08 +0300

    todo_list 04.04.24

---
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_9/lesson9.java
@@ -0,0 +1,20 @@
+/**
+ * java basic
+ * lesson 9
+ *
+ * @version 1.0 from 2024-04-04
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential_New.lesson_9;
+
+public class lesson9 {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
