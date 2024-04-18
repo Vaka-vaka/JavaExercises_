a9e1cebed0c47566e98b0f9fe6a966ac7f85355a
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-04-18 14:29:46 +0300

    lesson_9 18.04.24

--- JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_9/lesson9/FileManager.java
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_9/lesson9/FileManager.java
@@ -9,6 +9,7 @@
 package iT_VDN.java_Essential_New.lesson_9.lesson9;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileManager {
     public void createDirectory(String directoryName) {
@@ -23,4 +24,21 @@ public class FileManager {
             }
         }
     }
+
+    public void createFile(String directoryName, String fileName) {
+        File file = new File("src\\iT_VDN\\java_Essential_New\\lesson_9\\lesson9"
+                + directoryName, fileName);
+        if (file.exists()) {
+            System.out.println("Файл "  + fileName + " вже існує!");
+        } else {
+            try {
+                if (file.createNewFile()) {
+                    System.out.println("Файл " + " створено успішно!");
+                }
+            } catch (IOException e){
+           e.printStackTrace();
+        }
+
+    }
+  }
 }
