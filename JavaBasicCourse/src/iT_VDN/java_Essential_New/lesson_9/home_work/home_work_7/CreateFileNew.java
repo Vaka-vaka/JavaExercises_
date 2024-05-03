57110f4d58bad0ac70d53092add2caf99c52bffa
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-05-03 14:19:09 +0300

    home_work_9 03.05.24

---
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_9/home_work/home_work_7/CreateFileNew.java
@@ -0,0 +1,40 @@
+/**
+ * java basic
+ * home_work
+ *
+ * @version 1.0 from 2024-05-02
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential_New.lesson_9.home_work.home_work_7;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CreateFileNew {
+    public void newFile() {
+        String strPath = "", strName = "";
+
+        try {
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(System.in));
+            System.out.println("Enter the file name: ");
+
+            strName = br.readLine();
+            System.out.println("Enter the file path: ");
+
+            strPath = br.readLine();
+
+            File file = new File(  strPath +
+                    strName + " .txt");
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+// W:/IT/Intelli IDEA(save projects)/JavaExercises/JavaBasicCourse/" +
+//                    "src/iT_VDN/java_Essential_New/lesson_9/home_work/home_work_7/
---
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_9/home_work/home_work_7/MainCreateFileNew.java
@@ -0,0 +1,21 @@
+/**
+ * java basic
+ * home_work
+ *
+ * @version 1.0 from 2024-05-02
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential_New.lesson_9.home_work.home_work_7;
+
+public class MainCreateFileNew {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+        CreateFileNew createFileNew1 = new CreateFileNew();
+        createFileNew1.newFile();
+    }
+}
