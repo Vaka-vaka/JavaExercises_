fa6dd0070592cf2c458488f6bf307d0d0fd7da64
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2024-05-06 16:36:14 +0300

    home_work_9 06.05.24

---
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_9/home_work/home_work_7/BufferedReaderNew.java
@@ -0,0 +1,38 @@
+/**
+ * java basic
+ * home_work
+ *
+ * @version 1.0 from 2024-05-06
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential_New.lesson_9.home_work.home_work_7;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderNew {
+    private final String basePath;
+
+    public BufferedReaderNew(String basePath) {
+        this.basePath = basePath;
+    }
+
+    public void bufferedReaderNewMethod() {
+        File file = new File(basePath);
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(
+                    new FileReader(file));
+            String str;
+
+            while ((str = bufferedReader.readLine()) != null) {
+                System.out.println(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ JavaBasicCourse/src/iT_VDN/java_Essential_New/lesson_9/home_work/home_work_7/FileReaderNew.java
@@ -0,0 +1,32 @@
+/**
+ * java basic
+ * home_work
+ *
+ * @version 1.0 from 2024-05-06
+ * @author Valentyn Mozul
+ */
+
+package iT_VDN.java_Essential_New.lesson_9.home_work.home_work_7;
+
+import java.io.FileReader;
+
+public class FileReaderNew {
+    private final String basePath;
+
+    public FileReaderNew(String basePath) {
+        this.basePath = basePath;
+    }
+
+    public void fileReaderNewMethod() {
+        try {
+            FileReader fileReader = new FileReader(basePath);
+            int i;
+
+            while ((i = fileReader.read()) != -1) {
+                System.out.print((char) i);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
